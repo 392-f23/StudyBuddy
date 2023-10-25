f8aa7f4d053d45931c48300827e844d56ddb8db8
Chibu O <56417244+ChibuO@users.noreply.github.com>
2023-10-25 12:12:33 -0500

    added firebase

---
+++ src/utilities/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCS0fx98HtStiYYtV7V87eQW2jCcCNfjG8",
+  authDomain: "studybuddy-79445.firebaseapp.com",
+  projectId: "studybuddy-79445",
+  storageBucket: "studybuddy-79445.appspot.com",
+  messagingSenderId: "574789157411",
+  appId: "1:574789157411:web:c6b82d8b9ce7370de7e796"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
