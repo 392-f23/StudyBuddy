ee2884368e58690c6556a845e83a14228ce03fc3
murirop <panashemuriro2023@u.northwestern.edu>
2023-10-25 20:32:05 -0500

    Add material UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
-import { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 import { initializeApp } from "firebase/app";
+import { Grid } from "@mui/material";
 
 const App = () => {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        
-       
-        <p>
-          
-         
-        </p>
-      </header>
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+         <div>banner</div>
+        </Grid>
+        <Grid item xs={8}>
+          <div>rest of the screen</div>
+        </Grid>
+      </Grid>
     </div>
   );
 };
