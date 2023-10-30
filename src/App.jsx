c7ef25e9505c4e67f7273033314d75ef55371f02
Chibu O <56417244+ChibuO@users.noreply.github.com>
2023-10-29 22:20:59 -0500

    inserted courses and modals

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,23 @@ import Feed from "./components/Feed/Feed";
 import Schedule from "./components/Schedule/Schedule";
 import Profile from "./components/Profile/Profile";
 import "./App.css";
-import { Container} from "@mui/material";
+import { Container } from "@mui/material";
 import { CreatePost } from "./components/CreatePost/CreatePost";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useDbData } from "./utilities/firebase";
 
 const App = () => {
 
-  const [courses, result] = useDbData("/courses")
+  const [posts, result] = useDbData("/courses");
 
-  console.log(courses)
-  
   return (
     <Container maxWidth='sm'>
       <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Feed />}/>
-        <Route path='/schedule' element={<Schedule />}/>
-        <Route path='/profile' element={<Profile />}/>
-      </Routes>
+        <Routes>
+          <Route path='/' element={<Feed posts={posts} />} />
+          <Route path='/schedule' element={<Schedule />} />
+          <Route path='/profile' element={<Profile />} />
+        </Routes>
       </BrowserRouter>
     </Container>
   );
--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,63 +1,59 @@
-import { Container, Paper, Button, Chip } from "@mui/material";
+import { useState } from "react";
+import { Container } from "@mui/material";
 import { Stack } from "@mui/system";
-import { styled } from '@mui/material/styles';
-import './Feed.css'
 import Banner from "../Banner/Banner";
 import Header from "../Header/Header";
+import { PostItem } from "../PostItem/PostItem";
+import "./Feed.css";
+import ContactModal from "../ContactModal/ContactModal";
+import AvailabilityModal from "../AvailabilityModal/AvailabiltyModal";
+import InfoDialog from "../Dialog/Dialog";
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
 
-const Feed = () => {
-    return (
-      <Container maxWidth='sm'>
-        <Header />
-        <Container maxWidth='sm'> 
-          <Stack spacing={2}>
-              <Item className='post-item'>
-                  <h2 className='post-description'>HW2 Partner?</h2>
-                  <Chip className='post-course' size="small" label="CS 338" />
-                  <p className='post-text'>Stuck on HW2. HMU if u wanna work together!</p>
-                  <p className='post-name'>Anonymous</p>
-                  <p className='post-location'>Location: Remote</p>
-                  <div className='button-flex'>
-                      <Button variant="contained">Contact</Button>
-                      <Button variant="contained">Availability</Button>
-                  </div>
-              </Item>
-              <Item className='post-item'>
-                  <h2 className='post-description'>Cypress Tutorial</h2>
-                  <Chip className='post-course' size="small" label="CS 392" />
-                  <p className='post-text'>Looking for people to work on Cypress Tutorial with, is anyone not done?</p>
-                  <p className='post-name'>Anonymous</p>
-                  <p className='post-location'>Location: Remote</p>
-                  <div className='button-flex'>
-                      <Button variant="contained">Contact</Button>
-                      <Button variant="contained">Availability</Button>
-                  </div>
-              </Item>
-              <Item className='post-item'>
-                  <h2 className='post-description'>JS Exercises</h2>
-                  <Chip className='post-course' size="small" label="CS 392" />
-                  <p className='post-text'>Anyone doing the JavaScript exercises this week?</p>
-                  <p className='post-name'>Anonymous</p>
-                  <p className='post-location'>Location: Remote</p>
-                  <div className='button-flex'>
-                      <Button variant="contained">Contact</Button>
-                      <Button variant="contained">Availability</Button>
-                  </div>
-              </Item>
-          </Stack>
-        </Container>
-        <Banner />
-      </Container>
-    );
+
+const Feed = ({ posts }) => {
+  const [openContact, setOpenContact] = useState(false);
+
+  const handleClickOpenContact = () => {
+    setOpenContact(true);
   };
-  
+
+  const handleCloseContact = () => {
+    setOpenContact(false);
+  };
+
+  const [openAvailability, setOpenAvailability] = useState(false);
+
+  const handleClickOpenAvailability = () => {
+    setOpenAvailability(true);
+  };
+
+  const handleCloseAvailability = () => {
+    setOpenAvailability(false);
+  };
+
+  const [contact, SetContact] = useState({});
+
+
+  return (
+    <Container maxWidth='sm'>
+      <InfoDialog title={"Contact"} open={openContact} handleClose={handleCloseContact}>
+        <ContactModal contact={contact} />
+      </InfoDialog>
+      <InfoDialog title={"Availability"} open={openAvailability} handleClose={handleCloseAvailability}>
+        <AvailabilityModal contact={contact} />
+      </InfoDialog>
+      <Header />
+      <Container maxWidth='sm'>
+        <Stack spacing={2} className="feed-stack">
+          {posts && posts.map((item, index) =>
+            <PostItem key={index} post={item} handleOpenContact={handleClickOpenContact} handleOpenAvailability={handleClickOpenAvailability}/>
+          )}
+        </Stack>
+      </Container>
+      <Banner />
+    </Container>
+  );
+};
+
 export default Feed;
-  
