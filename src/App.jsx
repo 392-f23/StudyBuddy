4eaeaebbf7d1a016bce676e1adf78c97d69e651c
Anna Cai <annacai44@gmail.com>
2023-11-01 20:32:18 -0500

    made background purple and other UI enhancements

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,24 @@ import { CreatePost } from "./components/CreatePost/CreatePost";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useDbData } from "./utilities/firebase";
 import Header from "./components/Header/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   const [posts, result] = useDbData("/courses");
 
   return (
-    <Container maxWidth="sm">
-      <BrowserRouter>
+    <div>
       <Header />
-        <Routes>
-          <Route path="/" element={<Feed posts={posts} />} />
-          <Route path="/schedule" element={<Schedule />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/create_post" element={<CreatePost />} />
-        </Routes>
-      </BrowserRouter>
-    </Container>
+      <Container maxWidth="sm">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Feed posts={posts} />} />
+            <Route path="/schedule" element={<Schedule />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </BrowserRouter>
+      </Container>
+    </div>
   );
 };
 
--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -2,7 +2,9 @@ import { FormControl, Select, InputLabel, MenuItem } from "@mui/material";
 import { useState } from "react";
 import "./Filters.css";
 
-const Filters = ({ course, setCourse, mode, setMode }) => {
+import Form from "react-bootstrap/Form";
+
+function Filters({ course, setCourse, mode, setMode }) {
   const changeCourse = (event) => {
     setCourse(event.target.value);
   };
@@ -15,40 +17,85 @@ const Filters = ({ course, setCourse, mode, setMode }) => {
 
   return (
     <div className="filters">
-      <FormControl sx={{ m: 1, minWidth: 180 }} className="filter">
-        <InputLabel id="demo-simple-select-label">Course</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={course}
-          label="Course"
-          onChange={changeCourse}
-        >
-          <MenuItem value={"All"}>All</MenuItem>
-          {courses.map((courseName) => (
-            <MenuItem key={courseName} value={courseName}>
-              {courseName}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-
-      <FormControl sx={{ m: 1, minWidth: 120 }} className="filter">
-        <InputLabel id="demo-simple-select-label">Mode</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={mode}
-          label="Mode"
-          onChange={changeMode}
-        >
-          <MenuItem value={"Both"}>Both</MenuItem>
-          <MenuItem value={"In-Person"}>In-Person</MenuItem>
-          <MenuItem value={"Remote"}>Remote</MenuItem>
-        </Select>
-      </FormControl>
+      <Form.Select
+        className="filter"
+        onChange={changeCourse}
+        aria-label="Default select example"
+      >
+        <option value="All">All Courses</option>
+        {courses.map((courseName) => (
+          <option key={courseName} value={courseName}>
+            {courseName}
+          </option>
+        ))}
+      </Form.Select>
+      <Form.Select
+        className="filter"
+        onChange={changeMode}
+        aria-label="Default select example"
+      >
+        <option value="Both">Both</option>
+        <option value="Remote">Remote</option>
+        <option value="In-Person">In-Person</option>
+      </Form.Select>
     </div>
   );
-};
+}
 
 export default Filters;
+
+// const Filters = ({ course, setCourse, mode, setMode }) => {
+//   const changeCourse = (event) => {
+//     setCourse(event.target.value);
+//   };
+
+//   const changeMode = (event) => {
+//     setMode(event.target.value);
+//   };
+
+//   const courses = ["CHEM 330", "MATH 328", "CS 392", "CS 330"];
+
+//   return (
+//     <div className="filters">
+//       <FormControl sx={{ m: 1, minWidth: 180 }} className="filter">
+//         <InputLabel id="demo-simple-select-label">Course</InputLabel>
+//         <Select
+//           labelId="demo-simple-select-label"
+//           id="demo-simple-select"
+//           value={course}
+//           label="Course"
+//           onChange={changeCourse}
+//         >
+//           <MenuItem value={"All"}>All</MenuItem>
+//           {courses.map((courseName) => (
+//             <MenuItem key={courseName} value={courseName}>
+//               {courseName}
+//             </MenuItem>
+//           ))}
+//         </Select>
+//       </FormControl>
+
+//       <FormControl
+//         variant="filled"
+//         sx={{ m: 1, minWidth: 120 }}
+//         className="filter"
+//       >
+//         <InputLabel id="demo-simple-select-label">Mode</InputLabel>
+//         <Select
+//           sx={{ backgroundColor: "white" }}
+//           labelId="demo-simple-select-label"
+//           id="demo-simple-select"
+//           value={mode}
+//           label="Mode"
+//           onChange={changeMode}
+//         >
+//           <MenuItem value={"Both"}>Both</MenuItem>
+//           <MenuItem value={"In-Person"}>In-Person</MenuItem>
+//           <MenuItem value={"Remote"}>Remote</MenuItem>
+//         </Select>
+//       </FormControl>
+//     </div>
+//   );
+// };
+
+// export default Filters;
