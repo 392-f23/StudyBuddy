b745c1c15b52b9400e94517dbe22f949e6af5669
maseh90 <MasonHartweger2023@u.northwestern.edu>
2023-11-05 21:02:59 -0600

    user auth and database testing

--- src/components/Signin/Signin.jsx
+++ src/components/Signin/Signin.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import { Button, Stack } from "@mui/material";
 import { FirebaseSignIn, useAuth } from "../../utilities/firebase";
+import { useDbUpdate, useDbData } from "../../utilities/firebase";
+import { useEffect } from "react";
 
 export const Signin = () => {
   const [user] = useAuth();
 
-  console.log(user);
+  const [update, result] = useDbUpdate(`/users/${user ? user.uid : "unknown"}`);
+  const [getDbData, users] = useDbData(`/users/${user ? user.uid : "unknown"}`);
+
+
+  const signInGeneral = () => {
+
+  }
+
+  useEffect(() => {
+    // 1. Sign in (not currently in database) - need to check if in DB, then fill out form
+    // 2. Sign in (in database) - need to check if in DB
+    // 3. If signs in but does not fill out form, DO NOT PUT IN DB
+    if (user && user.uid) {
+      console.log(getDbData);
+    }
+  }, [user]);
+
   return (
     <Stack>
       <div>Logo</div>
